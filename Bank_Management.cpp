6697b234b93f5805bcc1cd7b27626106d0259eb1
Willy-Ze-Cacti <wangtianyi2222@gmail.com>
2023-06-05 03:32:06 -0400

    Update Bank_Management.cpp

--- Bank_Management.cpp
+++ Bank_Management.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <vector>
 #include <limits>
+#include <iomanip>
 using namespace std;
 
 class Account {
@@ -58,8 +59,19 @@ public:
         transaction_type[0] = type;
     }
 
+    void clear_transaction() {
+        for (int i = 0; i < 20; ++i) {
+            transaction_type[i] = "";
+            transaction_amount[i] = 0;
+        }
+    }
+
     // prints the transactions recorded including the type and amount to the given output stream
     void printTransaction(ostream& ostr) {
+        if (transaction_amount[0] == 0) {
+            ostr << "There are no transaction on record." << "\n" << endl;
+            return;
+        }
         int i = 0;
         ostr << "Most recent transactions:" << endl;
         while (transaction_amount[i] != 0) { 
@@ -70,7 +82,7 @@ public:
             } else {
                 ostr << "Balance change: " << transaction_amount[i];
             }
-            ostr << endl << endl;
+            ostr << "\n" << endl;
             i++;
         }
     }
@@ -81,178 +93,273 @@ public:
         ofstream summary(generatedFileName);
         summary << "Name: " << get_name() << endl;
         summary << "Card Number: " << get_number() << endl;
-        summary << "Current Balance: " << get_balance() << endl << endl;
+        summary << "Current Balance: " << get_balance() << "\n" << endl;
         printTransaction(summary);
         summary.close();
     }
-
 };
 
-Account CreateAccount() {
+void printAccountList(vector<Account>& listofAccount) {
+    int totalAccount = listofAccount.size();
+    if (totalAccount == 0) {
+        cout << "There are currently no account on record." << "\n" << endl;
+    } else {
+        cout << "\nList of account names:" << endl;
+        for (int i = 0; i < totalAccount; ++i) {
+            cout << listofAccount[i].get_name() << endl;
+        }
+    }
+}
+
+int AccountSelector(vector<Account>& listofAccount, const string& message) {
+    string aName;
+    int totalAccount = listofAccount.size();
+    while (1) {
+        cout << "\n" << message << endl;
+        getline(cin, aName);
+        while (aName.length() == 0) {
+            cout << "Please enter an account. " << endl;
+            getline(cin, aName);
+        }
+        if (aName == "-list") {
+            printAccountList(listofAccount);
+            continue;
+        }
+        for (int i = 0; i < totalAccount; ++i) {
+            if (listofAccount[i].get_name() == aName) {
+                return i;
+            }
+        }
+        cout << "The account name you entered does not exist. Type \"-list\" for a list of existing account(s).\n";
+    }
+}
+
+
+void CreateAccount(vector<Account>& listofAccount) {
     string aName;
     double aBalance;
     int aNumber;
+    int TotalAccount = listofAccount.size();
+    int LoopControl = 1;
 
-    while (1) {
-        cout << "Please enter the account holder's name:" << endl;
+    cout << "\nPlease enter the account holder's name:" << endl;
+    while (LoopControl) { 
         getline(cin, aName);
         if (aName.length() == 0) {
             cout << "Please enter a name." << endl;
             continue;
-        } else {
-            cout << "You entered: " << aName << endl
-                 << endl;
-            break;
-        }
+        } 
+        for (int i = 0; i <= TotalAccount; ++i) {
+            if (i == TotalAccount) {
+                cout << "You entered: " << aName << endl;
+                LoopControl = 0;
+                break;
+            }
+            if (listofAccount[i].get_name() == aName) {
+                cout << "\n" << aName << " already has an account. Please enter another account name." << endl;
+                break;
+            }
+        }  
     }
 
-    while (1) {
-        cout << "Please enter the account number:" << endl;
+    
+    LoopControl = 1;
+    while (LoopControl) {
+        cout << "\nPlease enter an account number:" << endl;
         cin >> aNumber;
-        if (!cin.fail()) {
-            cout << "You entered: " << aNumber << endl << endl;
-            break;
-        } else {
-            cout << "That was not a valid account number." << endl;
+        if (cin.fail() || !(cin.eof() || cin.peek() == '\n')) {
+            cout << "The account number cannot contain anything other than numbers.\n";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        } 
+        for (int i = 0; i <= TotalAccount; ++i) {
+            if (i == TotalAccount) {
+                cout << "You entered: " << aNumber << endl;
+                LoopControl = 0;
+                break;
+            }
+            if (listofAccount[i].get_number() == aNumber) {
+                cout << aNumber << " is already being used.\n";
+                break;
+            }
         }
     }
 
     while (1) {
-        cout << "Please enter the account's balance:" << endl;
+        cout << "\nPlease enter the account's balance:" << endl;
         cin >> aBalance;
-        if (cin.fail()) {
-            cout << "That was not a valid balance number." << endl;
+        if (cin.fail() || !(cin.eof() || cin.peek() == '\n')) {
+            cout << "That is not a valid balance number.\n";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');  
             continue;
         } else if (aBalance < 0){
-            cout << "Account balance cannot be negative." << endl;
+            cout << "Account balance cannot be negative.\n";
             continue;
         } else {
             cout << "You entered: " << aBalance << endl << endl;
             break;
         }
     }
-    return Account(aName, aBalance, aNumber);
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    listofAccount.push_back(Account(aName, aBalance, aNumber));
+    cout << "The account has been added!\n" << endl;
 }
 
-void AddTransaction (Account &account) {
+
+void AddTransaction (vector<Account>& listofAccount) {
+    string aName;
+    int totalAccount = listofAccount.size();
     double amount;
     string type;
+    int AccountIndex = AccountSelector(listofAccount, 
+    "Please enter the name of the that you'd like to make a transaction for:");
+    cout << "Making transaction for: " << listofAccount[AccountIndex].get_name() << "." << endl;
+    cout << "Be aware that the system only saves the most recent 20 transactions! "
+            "The oldest transaction record will be replaced if there are already 20 transactions saved." << endl;
+
     while (1) {
-        cout << "Please enter the type of this transaction:" << endl;
+        cout << "\nPlease enter the type of this transaction:" << endl;
         getline(cin, type);
         if (type.length() == 0) {
-            cout << "The transaction type cannot be empty." << endl;
+            cout << "The transaction type cannot be empty.\n";
             continue;
         } else {
-            cout << "You entered: " << type << endl << endl;
+            cout << "You entered: " << type << endl;
             break;
         }
     }
 
     while (1) {
-        cout << "Please enter the amount of this transaction (enter a negative number for withdrawal):" << endl;
+        cout << "\nPlease enter the amount of this transaction (enter a negative number for withdrawal):" << endl;
         cin >> amount;
-        if (cin.fail()) {
-            cout << "That was not a valid amount." << endl;
+        if (cin.fail() || !(cin.eof() || cin.peek() == '\n')) {
+            cout << "That was not a valid amount." << endl << endl;
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        } else if (account.get_balance() < -amount) {
+        } else if (amount < 0.01 && amount > -0.01) {
+            cout << "The transaction amount cannot be lower than 0.01$!";
+            continue;
+        } else if (listofAccount[AccountIndex].get_balance() < -amount) {
             cout << "the account does not have enough balance to complete the transaction. ";
-            cout << "current balance: " << account.get_balance() << endl;
+            cout << "current balance: " << listofAccount[AccountIndex].get_balance() << endl;
             continue;
         } else {
-            cout << "You entered: " << amount << endl << endl;
+            cout << "You entered: " << amount << endl;
             break;
         }
     }
-    account.add_transaction(type, amount);
-    account.change_balance(amount);
-    cout << "Transation made successful! Current account balance: " << account.get_balance() << endl;
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    listofAccount[AccountIndex].add_transaction(type, amount);
+    listofAccount[AccountIndex].change_balance(amount);
+    cout << "Transation successfully made! Current account balance: " << listofAccount[AccountIndex].get_balance() << endl << endl;
 }
 
+void AccountSummary(vector<Account>& listofAccount) {
+    int AccountIndex = AccountSelector(listofAccount, 
+    "Please enter the name of the account that you'd like to print:");
+    cout << "\n";
+    listofAccount[AccountIndex].printTransaction(cout);
+}
+
+void PrintAccountSummary(vector<Account>& listofAccount) {
+    string fileName;
+    int AccountIndex = AccountSelector(listofAccount, 
+    "Please enter the name of the account that you'd like to print:");
+    cout << "Generating account summary for: " << listofAccount[AccountIndex].get_name() << ".\n\n";
+    cout << "Please enter a name for the summary file:" << endl;
+    getline(cin, fileName);
+    listofAccount[AccountIndex].generateAccountSummary(fileName);
+    cout << "The account summary has been generated as " << fileName << ".txt!" << "\n" << endl;    
+}
+
+
+void ClearTransaction (vector<Account>& listofAccount) {
+    int AccountIndex = AccountSelector(listofAccount, 
+    "Please enter the name of the account that you'd like to clear the transaction for");
+    listofAccount[AccountIndex].clear_transaction();
+    cout << listofAccount[AccountIndex].get_name() << "'s transaction history has been cleared!" << "\n" << endl;
+}
+
+
+
 int main() {
+    cout << fixed;
+    cout << setprecision(2);
     vector<Account> listofAccount;
     string input;
-    int totalAccount = 0;
     cout << "Welcome! ";
     while(1) {
         cout << "Please enter your command below. Type \"help\" for more information." << endl;
         getline(cin, input);
+
         if (input == "create") {
-            listofAccount.push_back(CreateAccount());
-            totalAccount++;
-            cout << "The account has been added!" << endl;
+            CreateAccount(listofAccount);
         } 
         
         else if (input == "print account summary") {
-            string aName;
-            string fileName;
-            int AccountIndex = 0;
-            int LoopControl = 1;
-            while (LoopControl) {
-                cout << "Please enter the account name of the account you'd like to print:" << endl;
-                getline(cin, aName);
-                for (; AccountIndex < totalAccount; ) {
-                    if (listofAccount[AccountIndex].get_name() == aName) {
-                        LoopControl = 0;
-                       break;
-                    }
-                    AccountIndex++;
-                }
-                if (LoopControl) {
-                    cout << "The account name you entered does not exist." << endl;
-                }
+            if (listofAccount.size() == 0) {
+                cout << "There are no account on record. Please create an account first." << "\n" << endl;
+            } else {
+                PrintAccountSummary(listofAccount);
             }
-
-            cout << "Generating account summary for: " << aName << "." << endl;
-            cout << "Please enter a name for the summary file:" << endl;
-            getline(cin, fileName);
-            listofAccount[AccountIndex].generateAccountSummary(fileName);
-            cout << "The account summary has been generated as " << fileName << ".txt!" << endl << endl;
         } 
 
-        else if (input == "transaction") {
-            string aName;
-            int AccountIndex = 0;
-            int LoopControl = 1;
-            while (LoopControl) {
-                cout << "Please enter the account name you'd like to make a transaction:" << endl;
-                getline(cin, aName);
-                for (; AccountIndex < totalAccount; ) {
-                    if (listofAccount[AccountIndex].get_name() == aName) {
-                        LoopControl = 0;
-                        break;
-                    }
-                    AccountIndex++;
-                }
-                if (LoopControl) {
-                    cout << "The account name you entered does not exist." << endl;
-                }
+        else if (input == "account summary") {
+            if (listofAccount.size() == 0) {
+                cout << "There are no account on record. Please create an account first." << "\n" << endl;
+            } else {
+                AccountSummary(listofAccount);
             }
-
-            cout << "Making transaction for: " << aName << "." << endl;
-            AddTransaction(listofAccount[AccountIndex]);
         }
+
+        else if (input == "transaction") {
+            if (listofAccount.size() == 0) {
+                cout << "There are no account to add transaction to. Please create an account first." << "\n" << endl;
+            } else {
+                AddTransaction(listofAccount);
+            }
+        }
+
         /*
         else if (input == "delete") {
 
         }
+        */
 
         else if (input == "clear transaction") {
-
+            if (listofAccount.size() == 0) {
+                cout << "There are no account to add transaction to. Please create an account first." << "\n" << endl;
+            }
+            string confirmation;
+            cout << "\n" << "Are you sure? Cleared account transactions are not recoverable!\n";
+            while (1) {
+                cout << "Type \"yes\" or \"y\" to confirm, \"no\" or \"n\" otherwise." << endl;
+                getline(cin, confirmation);
+                if (confirmation == "yes" || confirmation == "y") {
+                    ClearTransaction(listofAccount);
+                    break;
+                } else if (confirmation == "no" || confirmation == "n") {
+                    break;
+                } else {
+                    cout << "Invalid input. ";
+                }
+            }
         }
-        */
         
+        else if (input == "") {
+            continue;
+        }
+
         else if (input == "quit" || input == "q") {
             break;
         } 
         
         else {
-            cout << "\"" << input << "\" is not a valid command." << endl;
+            cout << "\"" << input << "\" is not a valid command.\n" << endl;
             cin.clear();
         }
     }
